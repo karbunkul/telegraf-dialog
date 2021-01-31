2f3e346f1d3e5a29ea107a35ca5e00116095c94f
karbunkul <karbunkul@yourtask.ru>
2021-02-01 02:45:47 +0500

    middleware

--- lib/index.ts
+++ lib/index.ts
@@ -1,42 +1,6 @@
-import debug from "debug";
-import { Context } from "telegraf";
-import events from "events";
-import DialogHandler from "./handler";
-import { Dialog } from "./dialog";
-
-const logger = debug("telegraf-dialog:middleware");
-
 export { Dialog } from "./dialog";
 export { Topic, TopicContext } from "./topic";
 
-export default function (opts: {
-  dialogs: Dialog[];
-  storeKey?: string;
-  namespace: string;
-}) {
-  const eventEmitter = new events.EventEmitter();
+import dialog from "./middleware";
 
-  const dialogHandler = new DialogHandler({
-    dialogs: opts.dialogs,
-    eventEmitter,
-  });
-
-  return async (ctx: Context, next: Function) => {
-    const storeKey = `telegraf_dialog: ${opts.namespace}`;
-    const session: any = (ctx as any)[opts.storeKey ?? "session"];
-
-    if (!session.hasOwnProperty(storeKey)) {
-      logger("store key", storeKey);
-      session[storeKey] = {};
-      logger("init store");
-    }
-    dialogHandler.storage = session[storeKey];
-    eventEmitter.once("clear", () => {
-      session[storeKey] = {};
-    });
-
-    ctx.dialog = dialogHandler;
-    dialogHandler.ctx = ctx;
-    return dialogHandler.handle(ctx, next);
-  };
-}
+export default dialog;
---
+++ lib/middleware.ts
@@ -0,0 +1,40 @@
+import debug from "debug";
+import { Context } from "telegraf";
+import events from "events";
+import DialogHandler from "./handler";
+import { Dialog } from "./dialog";
+
+const logger = debug("telegraf-dialog:middleware");
+
+export default function (opts: {
+  dialogs: Dialog[];
+  storeKey?: string;
+  namespace: string;
+}) {
+  const eventEmitter = new events.EventEmitter();
+
+  const dialogHandler = new DialogHandler({
+    dialogs: opts.dialogs,
+    eventEmitter,
+  });
+
+  return async (ctx: Context, next: Function) => {
+    const storeKey = `telegraf_dialog: ${opts.namespace}`;
+    const session: any = (ctx as any)[opts.storeKey ?? "session"];
+
+    if (!session.hasOwnProperty(storeKey)) {
+      logger("store key", storeKey);
+      session[storeKey] = {};
+      logger("init store");
+    }
+    dialogHandler.storage = session[storeKey];
+    eventEmitter.once("clear", () => {
+      session[storeKey] = {};
+    });
+
+    // @ts-ignore
+    ctx.dialog = dialogHandler;
+    dialogHandler.ctx = ctx;
+    return dialogHandler.handle(ctx, next);
+  };
+}
